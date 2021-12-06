97d3fb3dd1b32aa7b5dc9268e3fa815c0de09034
Katie Sweatman <sweatmak@newschool.edu>
2021-12-06 17:28:23 -0500

    LanguageParser updated

--- LanguageParser.py
+++ LanguageParser.py
@@ -1,32 +1,112 @@
-import csv
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+from numpy import spacing
 import pandas as pd
 import nltk
-from nltk.corpus import stopwords
-import re
+import matplotlib.pyplot as plt
 
-#load datasets
-SurveyData=pd.read_csv('SurveyData.csv')
-FacData=pd.read_csv('Faculty.csv')
-StudentData=pd.read_csv('Student.csv')
-GradData=pd.read_csv('Graduate.csv')
-UGData=pd.read_csv('Undergraduate.csv')
+import collections, operator
+import csv
+from nltk.util import ngrams
+import seaborn as sns
+
+nltk.download('stopwords')
+#nltk.download('punkt')
 
 #Sample Question: "What feature(s) do you wish our website had?" in row [15]
-words=[]
-with open ('SurveyData.csv','r') as CSVFile:
-    for row in CSVFile:
-        csv_words=row[15].split(" ")
-        for i in csv_words:
-            words.append(i)
-word_count=[]
-for i in words:
-    x=words.count(i)
-    word_count.append((i,x))
-"""   if word not in word_count:
-            word_count[word]=0
-        word_count[word]=word_count[word]+1 """
 
-print(word_count)
+#import survey data and convert to nltk corpus
 
-#word_count=pd.Series(SurveyData["What feature(s) do you wish our website had?"]).str.extractall(r'*\w').count()
-#print(word_count)
+corpus = ''
+corpus_list = []
+file = open("SurveyData.csv","r",encoding="utf-8")
+data = csv.reader(file)
+
+for row in data:
+    corpus = corpus + row[15] + ' '
+    corpus_list.append(row[15])
+
+           
+file.close()
+
+#tokenize words, convert to lowercase
+tokens = nltk.word_tokenize(corpus.lower())
+
+# create stop_words variable from NLTK stopwords
+# create new list of filtered_tokens- tokens with stopwords removed
+
+stop_words = set(stopwords.words('english'))
+filtered_tokens = []
+
+for word in tokens:
+    if not word in stop_words:
+        filtered_tokens.append(word)
+
+# print length of corpus_list (Number of rows), tokens list (number of words), and filtered_tokens list (number of words minus stopwords)
+
+#print(len(corpus_list), len(tokens), len(filtered_tokens))
+
+# print list of all words
+#print(tokens)
+
+# print list of all words minus stopwords
+#print(filtered_tokens)
+
+# Transform word lists (tokens) into NLTK Text objects
+
+text = nltk.Text(tokens)
+
+filtered_text = nltk.Text(filtered_tokens)
+
+# Create a frequency distribution of all words (how often a word appears in text)
+
+freq_dist = FreqDist(tokens)
+
+freq_dist_filtered = FreqDist(filtered_tokens)
+
+#plot of top 25 tokens
+
+#print(freq_dist.plot(25, cumulative=False))
+
+#plot of top 25 filtered tokens
+
+#print(freq_dist_filtered.plot(25, cumulative=False))
+
+#clean up data based on initial plots
+
+filter_words = dict([(m, n) for m, n in freq_dist_filtered.items() if len(m) > 3 and m not in ['would','think','sure','like','know','sure','maybe','could','much','wish','better']])
+
+#for key in sorted(filter_words):
+ #   print("%s: %s" % (key, filter_words[key]))
+ 
+data_analysis = nltk.FreqDist(filter_words)
+#data_analysis.plot(25, cumulative=False)
+
+## Creating FreqDist for filtered_words, keeping the 25 most common tokens
+all_fdist = FreqDist(filter_words).most_common(25)
+
+## Conversion to Pandas series via Python Dictionary for easier plotting
+all_fdist = pd.Series(dict(all_fdist))
+
+
+#create bar plot from data in above cell using same layout
+## Setting figure, ax into variables
+fig, ax = plt.subplots(figsize=(10,10))
+
+ax.set_ylabel('Number of Occurrences')
+ax.set_xlabel('Word')
+plt.title('Top 25 Words in Survey Responses to  "What feature(s) do you wish our website had?"')
+plt.grid()
+
+## Seaborn plotting using Pandas attributes + xtick rotation for ease of viewing
+
+bar_plot = sns.barplot(x=all_fdist.index, y=all_fdist.values, ax=ax)
+ax.set_ylim(ymin=0)
+plt.xticks(rotation=45)
+plt.savefig("sitefeatures.png")
+
+# prints top n of collocates in text (how often words appear next to one another, in pairs)
+
+n = 25
+print(text.collocations(n))
+
--- Parser.py
+++ Parser.py
@@ -8,11 +8,11 @@ StudentData=pd.read_csv('Student.csv')
 GradData=pd.read_csv('Graduate.csv')
 UGData=pd.read_csv('Undergraduate.csv')
 
-#Single Answers-Did you find what you were looking for?
+#Single Answers-Did you find what you were looking for?-pie chart
 Answer=SurveyData.groupby("On your last visit to the library website, did you find what you were looking for? - Selected Choice")['On your last visit to the library website, did you find what you were looking for? - Selected Choice'].count()
 print(Answer)
 
-#Multiple Answers-What other library websites do you use?
+#Multiple Answers-What other library websites do you use?-bar graph
 
 NYPL=pd.Series(SurveyData['What other library websites do you visit? - Selected Choice']).str.extractall("(New York Public Library)").count()
 print(NYPL)
@@ -29,14 +29,13 @@ print(Other)
 NA=pd.Series(SurveyData['What other library websites do you visit? - Selected Choice']).str.extractall("(None/Not Applicable)").count()
 print(NA)
 
-#Site Ratings
+#Site Ratings-graph as stacked percentage bar chart
 
 Clarity_Rating=SurveyData.groupby("Rate the following aspects of the website - Information Clarity")['Rate the following aspects of the website - Information Clarity'].count()
 Clarity_Rating_Perc=(Clarity_Rating/len(SurveyData))*100
-print(Clarity_Rating_Perc)
-#print(Clarity_Rating.mean)
 Nav_Rating=SurveyData.groupby("Rate the following aspects of the website - Navigability")['Rate the following aspects of the website - Navigability'].count()
-
+Nav_Rating_Perc=(Nav_Rating/len(SurveyData))*100
 GetHelp_Rating=SurveyData.groupby("Rate the following aspects of the website - Getting Help")['Rate the following aspects of the website - Getting Help'].count()
-
+GetHelp_Rating_Perc=(GetHelp_Rating/len(SurveyData))*100
 Rating=SurveyData.groupby("Rate the following aspects of the website - Overall Experience")['Rate the following aspects of the website - Overall Experience'].count()
+Rating_Perc=(Rating/len(SurveyData))*100
